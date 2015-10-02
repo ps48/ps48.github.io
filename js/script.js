cb394fd220e45d3ec3b3527ec1eec7986e793ddc
unknown <pshenoy36@gmail.com>
2015-10-02 23:49:56 +0530

    project images added resizing fixed

--- js/script.js
+++ js/script.js
@@ -6,6 +6,58 @@ $(document).ready(function(){
       }, 1000);
   });
 
+  $('.purple').click(function() {
+  	if(this==s1)
+  	{
+      $('#mtitle').html("Small Image Processing Scripts");
+      $('#mbody').html("Sip");
+    }
+    else
+    if(this==s2)
+    {
+      $('#mtitle').html("Life Plus");
+      $('#mbody').html("Sip");
+    }
+    else
+    if(this==s3)
+    {
+      $('#mtitle').html("Genre-ist");
+      $('#mbody').html("Sip");
+    }
+    else
+    if(this==s4)
+    {
+      $('#mtitle').html("Aaruush'15	Website");
+      $('#mbody').html("Sip");
+    }
+    else
+    if(this==s5)
+    {
+      $('#mtitle').html("SRM Walk");
+      $('#mbody').html("Sip");
+    }
+    else
+    if(this==s6)
+    {
+      $('#mtitle').html("Aaruush App");
+      $('#mbody').html("Sip");
+    }
+    else
+    if(this==s7)
+    {
+      $('#mtitle').html("SRM SE");
+      $('#mbody').html("Sip");
+    }
+    else
+    {
+      $('#mtitle').html("Report Portal");
+      $('#mbody').html("Sip");
+    }
+
+
+  });
+
   
 
 });
+
